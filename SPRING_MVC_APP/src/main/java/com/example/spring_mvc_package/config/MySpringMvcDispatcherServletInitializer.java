7054855e3c48f31b79e567e3c05864c6b1cf03cb
strBCL1 <work.artem.popov@gmail.com>
2022-04-07 20:48:55 +0200

    Lesson 16 - Spring MVC configuration using Java code

---
+++ SPRING_MVC_APP/src/main/java/com/example/spring_mvc_package/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package com.example.spring_mvc_package.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
